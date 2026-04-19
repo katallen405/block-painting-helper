426639f05475bc82477b6c30c7657a3f7606e5ed
katallen405 <95441393+katallen405@users.noreply.github.com>
2026-04-19 13:52:22 -0400

    bph_interfaces fixed dependencies

---
+++ bph_pickmeup/bph_pickmeup/__init__.py
@@ -0,0 +1,3 @@
+# in /ros_ws/src/block-painting-helper/bph_pickmeup/bph_pickmeup/__init__.py
+from bph_pickmeup.bph_pickmeup_client import BphPickmeupClient
+from bph_pickmeup.bph_pickmeup_action_server import BphPickmeupServer
